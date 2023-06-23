713356060fe570b6a4df30006fe65a208b341069
Vladimir <shokilak@gmail.com>
2023-06-24 01:31:54 +0600

    feat: added navigation

---
+++ frontend/src/components/navigation/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "@/styles/navigation.module.css"
+
+const Logo = () => {
+    return (
+        <div>
+            Logo
+        </div>
+    );
+};
+
+export default Logo;
