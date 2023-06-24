edb17e786c3a1871d9ff91efd5f1b46e2473d8ce
Vladimir <shokilak@gmail.com>
2023-06-25 00:44:08 +0600

    Update index.tsx

--- frontend/src/pages/auth/index.tsx
+++ frontend/src/pages/auth/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Auth from "@/components/auth/Auth";
+import LayoutGradient from "@/components/LayoutGradient";
 
 const Index = () => {
     return (
         <>
-            <Auth/>
+            <LayoutGradient>
+                <Auth/>
+            </LayoutGradient>
         </>
     );
 };
